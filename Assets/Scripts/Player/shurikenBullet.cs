31dfb536f030e8cf9b433fc83b35cba8fda1012f
southsafe0123 <naman.nguyen49002@gmail.com>
2023-11-06 22:04:46 +0700

    fix shuriken, map3 area1

--- Assets/Scripts/Player/shurikenBullet.cs
+++ Assets/Scripts/Player/shurikenBullet.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class shurikenBullet : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public float shootingSpeed;
     bool isHitSth = false;
-    public Vector3 shootingDirection;
     public Animator anim;
     // Start is called before the first frame update
     void Start()
@@ -19,11 +17,7 @@ public class shurikenBullet : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (!isHitSth)
-        {
-            rb.velocity = Vector3.Slerp(transform.position, shootingDirection.normalized, 1) * shootingSpeed;
-        }
-        else
+        if (isHitSth)
         {
             rb.velocity = Vector2.zero;
         }
