925f6e39b124231a0783e22166e714fc1c4d9198
An-Panda2010 <dangbinhan2505@gmail.com>
2023-11-09 16:02:53 +0700

    Update VerticalCamera.cs

--- Assets/Scripts/Map/VerticalCamera.cs
+++ Assets/Scripts/Map/VerticalCamera.cs
@@ -13,7 +13,7 @@ public class VerticalCamera : MonoBehaviour
     // Update is called once per frame
     private void Start()
     {
-        PlayerMovement.mainCamera = Camera.main;
+        PlayerMovement.mainCamera = gameObject.GetComponent<Camera>();
         target = GameObject.Find("player").transform;
         enableLoadscene.SetActive(true);
     }
