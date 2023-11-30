d88dc6655abf43d93d655f219f56e21fafae9ebd
HoangKM4 <150126109+HoangKM4@users.noreply.github.com>
2023-11-30 09:54:15 +0700

    music and boss

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+
+    public AudioClip background;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+
+}
