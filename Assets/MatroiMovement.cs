5dc4175e3696843b223dafac41fa7d814e83f241
cuongncps02092 <cuongncps02092@fpt.edu.vn>
2023-11-30 09:55:03 +0700

    update Matroi

--- Assets/MatroiMovement.cs
+++ Assets/MatroiMovement.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MatroiMovement : MonoBehaviour
 {
     public MonsterTriggerRange range;
     public float monsSpeed;
     private Rigidbody2D rb;
     public GameObject bulletPrefab; // Prefab của đạn
-    public float bulletSpeed = 5f; // Tốc độ của đạn
-    public float knockBackDuration;
+    public Transform bulletPos; //
+    //public float bulletSpeed; // Tốc độ của đạn
     private bool hasShot = false;
+
+    private Vector2 targetPosition; // Vị trí mới của kẻ địch
+    public float moveSpeed; // Tốc độ di chuyển của kẻ địch
 
 
     // Start is called before the first frame update
@@ -22,28 +26,50 @@ public class MatroiMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RotationToPlayer();
         if (range.isPlayerInZone && !hasShot)
         {
-            RotationToPlayer();
             bullet();
+            MoveToRandomPosition();
         }
+
+        // Di chuyển kẻ địch đến vị trí mới
+        if(hasShot)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+       
     }
 
-    private void bullet()
+    void bullet()
     {
-        Vector2 direction = (range.player.position - transform.position).normalized;
-        GameObject obj = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-        obj.GetComponent<Rigidbody2D>().velocity = direction.normalized * bulletSpeed;
+        //Vector3 direction = (range.player.position - transform.position).normalized;
+        //GameObject obj = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        //obj.GetComponent<Rigidbody2D>().velocity = direction.normalized * bulletSpeed;
+        //transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+
+        Instantiate(bulletPrefab, bulletPos.position, Quaternion.identity);
+
         hasShot = true; // Đánh dấu đã bắn đạn
         StartCoroutine(DelayShot());
     }
 
     IEnumerator DelayShot()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(7);
         hasShot = false;
         StopCoroutine(DelayShot());
     }
+
+    void MoveToRandomPosition()
+    {
+        float randomX = Random.Range(-8f, 8f); // Chỉnh giới hạn di chuyển theo trục X
+        float randomY = Random.Range(-4f, 4f); // Chỉnh giới hạn di chuyển theo trục Y
+
+        targetPosition = new Vector2(randomX, randomY); // Tạo tọa độ mới
+        
+    }
+
 
     private void RotationToPlayer()
     {
---
+++ Assets/fireBulletScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireBulletScript : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rd;
+    public float speedBullet = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, direction); // Xoay viên đạn về phía người chơi
+        rd.velocity = new Vector2(direction.x, direction.y).normalized * speedBullet;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
