3bb44aba3c1fabaee6826a8296db0ce6db62426b
DoQuangLoi <116474144+DoQuangLoi@users.noreply.github.com>
2023-12-02 14:41:17 +0700

    Login_successs

---
+++ Assets/Scripts/API/GameAccount.cs
@@ -0,0 +1,9 @@
+
+using UnityEngine;
+
+public class GameAccount
+{
+    public string _id;
+    public int role;
+    public string name;
+}
--- Assets/Scripts/API/Login.cs
+++ Assets/Scripts/API/Login.cs
@@ -1,44 +1,75 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 using TMPro;
 using UnityEngine.Networking;
 using static UnityEngine.Networking.UnityWebRequest;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class Login : MonoBehaviour
 {
     [SerializeField] private string authenticationEndpoint = "http://192.168.68.125:8686/users/loginGame";
     [SerializeField] private TMP_InputField usernameInputField;
     [SerializeField] private TMP_InputField passwordInputField;
+    [SerializeField] private TextMeshProUGUI alertText;
+    [SerializeField] private Button loginButton;
+
+    public void clickRegister() {
+        SceneManager.LoadScene("Register_Scene");
+    }
 
     public void OnloginClick() {
+
+        alertText.text = "Sign in...";
+        loginButton.interactable = false;
         StartCoroutine(TryLogin());
     }
     private IEnumerator TryLogin() {
+
+
         string username = usernameInputField.text;
         string password = passwordInputField.text;
-        
-        UnityWebRequest request =  UnityWebRequest.Get($"{authenticationEndpoint}?email={username}&password={password}");
-        var handler = request.SendWebRequest();
-       
-        float startTime = 0.0f;
-        while (!handler.isDone) {
-            startTime += Time.deltaTime;
-            if (startTime > 10.0f) {
-                break;
+        if (username.Equals("") || password.Equals("")) {
+            alertText.text = "Không để trống...";
+            loginButton.interactable = true;
+        }
+        else {
+            UnityWebRequest request = UnityWebRequest.Get($"{authenticationEndpoint}?email={username}&password={password}");
+            var handler = request.SendWebRequest();
+
+            float startTime = 0.0f;
+            while (!handler.isDone)
+            {
+                startTime += Time.deltaTime;
+                if (startTime > 10.0f)
+                {
+                    break;
+                }
+
+                yield return null;
             }
-            
-            yield return null;
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+
+                loginButton.interactable = false;
+                GameAccount gameAccount = JsonUtility.FromJson<GameAccount>(request.downloadHandler.text);
+
+                alertText.text = "welcome..."+((gameAccount.role < 1 ) ? gameAccount.name : "Admin");
+                 
+                Debug.Log(request.downloadHandler.text);
+            } else if (request.result== UnityWebRequest.Result.ConnectionError) {
+
+                alertText.text = "Kiểm tra kết nối...";
+                loginButton.interactable = true;
+            }
+            else
+            {
+                alertText.text = "Kiểm tra tài khoản hoặc mật khẩu...";
+                loginButton.interactable = true;
+            }
         }
-        
-        if (request.result == UnityWebRequest.Result.Success)
-        {
-            Debug.Log(request.downloadHandler.text);
-        }
-        else
-        {
-            Debug.Log(request.result);
-        }
+
 
 
         yield return null;
