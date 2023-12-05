dd51074e9c546599d2ebf42afd8d683a2eb3c91c
southsafe0123 <naman.nguyen49002@gmail.com>
2023-12-05 08:38:58 +0700

    endingscene fix

---
+++ Assets/storyManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class storyManager : MonoBehaviour
+{
+
+    public void endScene()
+    {
+        LoadScene.scenePlayerIn = 1;
+        GameObject.Find("LoadScene").GetComponent<LoadScene>().LoadSceneNum(4);
+    }
+}
